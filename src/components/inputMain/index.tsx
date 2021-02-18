90f7592e48bcbc13a8b59c05a29b4fc6300ea9f9
Evgeny <evgeny.profet@gmail.com>
2021-02-18 16:24:10 +0300

    refactor(components): added materialUI into  component

--- src/components/inputMain/index.tsx
+++ src/components/inputMain/index.tsx
@@ -1,11 +1,34 @@
 import React, { FormEvent, useState } from 'react';
+import { Button, Container, Input, InputLabel, MenuItem, Select } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      maxWidth: 620,
+      display: 'flex',
+      flexDirection: 'row',
+      flexBasis: '200px',
+      justifyContent: 'center',
+      background: '#33509926',
+      borderRadius: '8px',
+      fontStyle: 'white',
+      padding: '10px',
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
 
 const InputMain: React.FC = () => {
   const [FormState, setFormState] = useState({
     input: '',
-    houseType: '',
-    persons: '1',
+    houseType: 'studio',
+    persons: '1 person',
   });
+  const classes = useStyles();
 
   const handleChange = (e: string, formField: string) => {
     if (formField === 'input') {
@@ -32,31 +55,48 @@ const InputMain: React.FC = () => {
   };
 
   return (
-    <div className="form-main-group">
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          value={FormState.input}
-          onChange={(event) => handleInputChange(event.target.value)}
-          placeholder="Введите адрес или фамилию арендодателя..."
-        />
-        <select
-          name="houseType"
-          onChange={(event) => handleChange(event.target.value, 'houseType')}
-        >
-          <option defaultValue="empty">выберите</option>
-          <option value="studio">Студия</option>
-          <option value="oneRoom">Однокомнатная</option>
-          <option value="twoRoom">2х комнатная</option>
-        </select>
-        <select onChange={(event) => handleChange(event.target.value, 'persons')}>
-          <option defaultValue="1 person">1</option>
-          <option value="2 person">2</option>
-          <option value="family">Семья</option>
-        </select>
-        <button type="submit">Найти</button>
+    <Container>
+      <h1>Ищете жилье?</h1>
+      <form onSubmit={handleFormSubmit} className={classes.formControl}>
+        <div style={{ width: 'auto', padding: '10px' }}>
+          <InputLabel id="input-adress">Адрес</InputLabel>
+          <Input
+            type="text"
+            value={FormState.input}
+            onChange={(event) => handleInputChange(event.target.value)}
+            placeholder="Введите адрес или фамилию арендодателя..."
+          />
+        </div>
+        <div style={{ width: 'auto', padding: '10px' }}>
+          <InputLabel id="select-house">Тип жилья</InputLabel>
+          <Select
+            labelId="select-house"
+            name="houseType"
+            value={FormState.houseType}
+            onChange={(event) => handleChange(event.target.value as string, 'houseType')}
+          >
+            <MenuItem value="studio">Студия</MenuItem>
+            <MenuItem value="oneRoom">Однокомнатная</MenuItem>
+            <MenuItem value="twoRoom">2х комнатная</MenuItem>
+          </Select>
+        </div>
+        <div style={{ width: 'auto', padding: '10px' }}>
+          <InputLabel id="select-persons">Кол-во человек</InputLabel>
+          <Select
+            labelId="select-persons"
+            value={FormState.persons}
+            onChange={(event) => handleChange(event.target.value as string, 'persons')}
+          >
+            <MenuItem value="1 person">1 человек</MenuItem>
+            <MenuItem value="2 person">2 человека</MenuItem>
+            <MenuItem value="family">Семья</MenuItem>
+          </Select>
+          <Button variant="contained" color="primary" type="submit">
+            Найти
+          </Button>
+        </div>
       </form>
-    </div>
+    </Container>
   );
 };
 
