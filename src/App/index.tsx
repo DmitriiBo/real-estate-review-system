de646a84ea6871d60a9309e28db3553225943ebd
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2021-03-24 18:17:23 +0300

    feat(components): добавь компонент AutoSlider
    Добавь компонент с автоматическим показом последних отзывов в виде слайдера
    
    BREAKING CHANGE:

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import '@fontsource/roboto';
 
+import AutoSlider from '../components/AutoSlider';
 import Header from '../components/Header';
 import Reviews from '../components/Reviews';
 import Search from '../components/Search';
+import { mockReviews } from '../mocks/review-mock-data';
 
 import { cnApp } from './cn-app';
 
@@ -15,6 +17,7 @@ export const App: React.FC = () => {
     <div className={cnApp()}>
       <p className={cnApp('Title')}>Real Estate Review System</p>
       <Header title="Main" />
+      <AutoSlider reviews={mockReviews} />
       <Search />
       <Reviews />
     </div>
